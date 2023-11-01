b0ff2af270d79763722ce0af69ecb750bb83630c
JB <bradburyjesse7@gmail.com>
2023-10-31 22:46:49 -0700

    updated README and added outline in JS file

--- script.js
+++ script.js
@@ -10,5 +10,18 @@ function writePassword() {
 
 }
 
+// OUTLINE
+// I want to prompt the user the length they want their password
+// if it bellow 8 or above 128, tell them the paramaters. 
+// I want to store their answer
+// Prompt the use if they want lowercase
+// Prompt the use if they want UPPERCASE
+// Prompt the use if they want numbers
+// Prompt the use if they want special car
+
+// Profit
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
