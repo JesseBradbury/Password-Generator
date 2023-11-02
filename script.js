d43f24145a884ff600dcdec86ebc286ee75c12e3
JB <bradburyjesse7@gmail.com>
2023-11-02 15:11:06 -0700

    Added psuedo code for the application. Created prompts for user. Added character lists for algorythm.

--- script.js
+++ script.js
@@ -10,18 +10,42 @@ function writePassword() {
 
 }
 
+var lowerList = "abcdefghijklmnopqrstuvwxyz";
+var upperList = lowerList.toUpperCase();
+var numberList = "1234567890"
+var specialList = "!@#$%^&*()"
+
+
+function generatePassword() {
+
+
 // OUTLINE
 // I want to prompt the user the length they want their password
 // if it bellow 8 or above 128, tell them the paramaters. 
+
+var length = prompt("How many characters would you like your password to be?");
+console.log(length);
 // I want to store their answer
-// Prompt the use if they want lowercase
-// Prompt the use if they want UPPERCASE
-// Prompt the use if they want numbers
-// Prompt the use if they want special car
+// Prompt the user if they want lowercase
+var lower = confirm("Would you like to use lower case letters?")
+console.log(lower);
+// I want to store their answer
+// Prompt the user if they want UPPERCASE
+var upper = confirm("Would you like to use upper case letters?")
+console.log(upper);
+// I want to store their answer
+// Prompt the user if they want numbers
+var number = confirm("Would you like to use numbers?")
+console.log(number);
+// I want to store their answer
+// Prompt the user if they want special char
+var special = confirm("Would you like to use special characters?")
+console.log(special);
+// I want to store their answer
 
-// Profit
+// Write an algorythm that creates a random string, based on the answers given in the above prompts. 
 
 
-
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
