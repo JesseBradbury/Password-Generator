57257f8232793cb0a7675d228acc81813b8ccc8f
JB <bradburyjesse7@gmail.com>
2023-11-03 23:20:07 -0700

    password generator functions :) Still need to update README.

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Arrays to use for generate password
@@ -27,16 +26,12 @@ var specialArray = specialList.split("");
 
 // This is our generate password function
 function generatePassword() {
-
-
-  // OUTLINE
-  // I want to prompt the user the length they want their password
-  // if it bellow 8 or above 128, tell them the paramaters. 
-
+  
+  // this do/while loop ensures that the user puts a value that is between 8 and 128. If they dont, it alerts them of the paramaters and returns the app.
   do {
     var length = prompt("How many characters would you like your password to be?");
     var parsedLength = parseInt(length);
-
+    
     if (parsedLength >= 8 && parsedLength <= 128 && !isNaN(parsedLength)) {
       console.log("Valid input: " + parsedLength);
     }
@@ -46,35 +41,28 @@ function generatePassword() {
     };
   }
   while (!(parsedLength >= 8 && parsedLength <= 128 && !isNaN(parsedLength)));
-  // I want to store their answer
-  // Prompt the user if they want lowercase
+  
+  // These are the promts for the user to answer
   var lower = confirm("Would you like to use lower case letters?")
   console.log(lower);
-  // I want to store their answer
-  // Prompt the user if they want UPPERCASE
+ 
   var upper = confirm("Would you like to use upper case letters?")
   console.log(upper);
-  // I want to store their answer
-  // Prompt the user if they want numbers
+ 
   var number = confirm("Would you like to use numbers?")
   console.log(number);
-  // I want to store their answer
-  // Prompt the user if they want special char
+  
   var special = confirm("Would you like to use special characters?")
-  console.log(special);
-  // I want to store their answer
-
-  // Write an algorythm that creates a random string, based on the answers given in the above prompts. 
-
-
+  console.log(special); 
+  
+  // This is the array that will contain our random password.
   let randomPass = [];
-
+  
+  // This is our loop for the password generator based on the length the user provided. 
   for (let index = 0; index < length; index++) {
-    // This works for just the lowercase Array 
-    // var randomChar = lowerArray[Math.floor(Math.random()*lowerArray.length)];
-    // randomPass.push(randomChar);
+    // This is the array that will contain all of the caracter strings that are selected by the user. 
     let selectedArrays = [];
-
+    // If the user selects okay, these push the arrays to the randomPass array.
     if (lower) {
       selectedArrays.push(lowerArray)
     }
@@ -87,20 +75,29 @@ function generatePassword() {
     if (special) {
       selectedArrays.push(specialArray)
     }
-
+    // This randomizes the array, and then chooses a random character from the array, and loops that pattern by the length the user selected. 
     if (selectedArrays.length > 0) {
       let randomArray = selectedArrays[Math.floor(Math.random() * selectedArrays.length)];
       let randomChar = randomArray[Math.floor(Math.random() * randomArray.length)];
       randomPass.push(randomChar);
     }
+    // This ensures the user selected atleast one character type, if they did not it alerts them and returns the app. 
     else {
       alert("Please choose atleast one character type.");
       return;
     }
-
-    let conPas = randomPass.join("");
-    console.log(conPas);
   }
+  // This joins the array into a string
+  let conPas = randomPass.join("");
+  console.log(conPas);
+  
+  // This updates the HTML element with the conPas string
+  let passwordElement = document.getElementById("password");
+  passwordElement.value = conPas;
+
+  return conPas;
+  
 }
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+
+// This is the event listener for the generate button
+generateBtn.addEventListener("click", writePassword);
